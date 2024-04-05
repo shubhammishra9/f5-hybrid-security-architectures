c8bf36d130a708d2333dcfe7d00d7a6fe40a0c45
Shubham Mishra <90624610+shubhammishra9@users.noreply.github.com>
2024-04-05 14:54:14 +0530

    updates

--- xc/xc_loadbalancer.tf
+++ xc/xc_loadbalancer.tf
@@ -276,14 +276,14 @@ resource "volterra_http_loadbalancer" "lb_https" {
           }
           flow_label {
             authentication {
-              login {
+              login #{
                 #transaction_result {
                   #failure_conditions {
                     #status = "401"
                   #}
                 #}
-                disable_transaction_result = true
-              }
+                #disable_transaction_result = true
+              #}
             }
           }
         }
